9d4ebc3ff0fa6ae50cbd9b20312bcdd46e280b4d
ROOterDairyman <dairyman@rfactory.org>
2014-12-09 18:39:26 -0800

    Commit files

---
+++ mbim-dev.h
@@ -0,0 +1,24 @@
+/*
+ * umbim
+ * Copyright (C) 2014 John Crispin <blogic@openwrt.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _MBIM_DEV_H__
+#define _MBIM_DEV_H__
+
+extern uint8_t mbim_buffer[MBIM_BUFFER_SIZE];
+extern int no_close;
+
+int mbim_send(void);
+void mbim_open(const char *path);
+
+#endif
