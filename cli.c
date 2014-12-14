93b7bd14e969b2c7fd4137ab1f946508537cb106
ROOterDairyman <dairyman@rfactory.org>
2014-12-13 19:14:41 -0800

    check telenumcnt

--- cli.c
+++ cli.c
@@ -168,7 +168,7 @@ mbim_subscriber_response(void *buffer, int len)
 	printf("  subscriberid: %s\n", subscriberid);
 	if (le32toh(state->readyinfo) & MBIM_READY_INFO_FLAG_PROTECT_UNIQUE_ID)
 		printf("  dont display subscriberID: 1\n");
-
+	printf("  telenumcnt: %d\n", le32toh(state->telephonenumberscount));
 /*	for (nr = 0; nr < le32toh(state->telephonenumberscount); nr++) {
 		struct mbim_string *str = buffer + le32toh(state->telephonenumbers) + (nr * sizeof(struct mbim_string));
 		char *number = mbim_get_string(str, buffer);
